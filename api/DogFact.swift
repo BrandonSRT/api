a7e27c473cd3b2524771cc32d1bfe556e3a5d541
Brandon <brandon.se2009@gmail.com>
2021-09-11 14:02:03 -0400

    Initial Commit

---
+++ api/DogFact.swift
@@ -0,0 +1,12 @@
+//
+//  DogFact.swift
+//  api
+//
+//  Created by user203110 on 9/11/21.
+//
+
+import Foundation
+struct DogFact: Codable {
+let fact: String
+let id=UUID()
+}
---
+++ api/apiApp.swift
@@ -0,0 +1,17 @@
+//
+//  apiApp.swift
+//  api
+//
+//  Created by user203110 on 9/11/21.
+//
+
+import SwiftUI
+
+@main
+struct apiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
